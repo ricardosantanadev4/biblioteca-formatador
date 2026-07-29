b7a529ad9bbdd8c5f1c6292a9cde11de4d25bb1b
ricardosantanadev4 <ricardosantanadev4@zarpar.app>
2026-07-29 18:07:21 -0300

    style: remover console.log

--- index.js
+++ index.js
@@ -6,11 +6,7 @@
  */
 function formatName (name) {
 
-  console.log('Iniciando function formatName');
-
   const partes = name.toLocaleLowerCase().split(' ');
-
-  console.log(`Palavras: ${partes}`);
 
   const formatedName = partes.map(p => p.charAt(0).toUpperCase() + p.slice(1)).join(' ');
 
